fa61756e6c4e142c96729e195258edb01f4b9925
Eric Cranston <cracell@gmail.com>
2018-02-01 11:12:17 -0800

    First pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+orders = 10.times do
+  Order.create(
+
+  )
+end
+
+def generate_order
+  Order.create(
+    order_number: '123',
+    delivery_date: Time.now,
+    destination_item_name: 'Whey Liquid DCCA',
+    item_price_cents: '25',
+    item_quantity: '24.0',
+    source_name: 'Western Milling - Liquid Direct',
+    order_status: 'shipped'
+  )
+end
+
+def generate_integration_order(order)
+  IntegrationOrder.create(
+    order: order,
+    export_state: 'exported'
+  )
+end
